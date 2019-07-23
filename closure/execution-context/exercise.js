a026988c6c675dee38fc1c39a07d094cc05fe714
Chetan Kumar <chetansain86@gmail.com>
2019-07-23 15:12:37 +0530

    Complete first assigment of closure

--- closure/execution-context/exercise.js
+++ closure/execution-context/exercise.js
@@ -18,9 +18,18 @@ var counter = (function() {
   };
 })();
 
-counter.value();
-counter.increment();
-counter.increment();
-counter.value();
-counter.decrement();
-counter.value();
+counter.value(); // 0
+counter.increment(); // 1
+counter.increment(); // 2
+counter.value(); // 2
+counter.decrement(); // 1
+counter.value(); // 1
+
+/*
+  counter.value(); Returns the value of the pirvateCounter 0
+  counter.increment(); Increments the privateCounter's value by 1 and returns undefined
+  counter.increment(); Increments the privateCounter's value by 1 and returns undefined
+  counter.value(); Returns the value of the pirvateCounter 2
+  counter.decrement(); Decrements the privateCounter's value by 1 and returns undefined
+  counter.value(); Returns the value of the pirvateCounter 1
+*/
